fab819e85bded0d710cc312094e4f2e728942fdd
Aditya Khoirul Anam <adit@ditya.dev>
2022-09-15 00:56:41 +0700

    feat: more descriptive usage on create command

--- internal/cmd/create.go
+++ internal/cmd/create.go
@@ -13,7 +13,7 @@ import (
 var createWithReverseFile bool
 
 var createCmd = &cobra.Command{
-	Use:   "create",
+	Use:   "create <name>",
 	Short: "Create a new migration file",
 	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
